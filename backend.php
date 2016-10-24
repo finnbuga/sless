bcfc26d54c885eb8e7ad3dc79f724f557cbb9548
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-10-24 12:05:43 +0100

    Remove sidebar: Header

--- backend.php
+++ backend.php
@@ -25,15 +25,6 @@ function lfr_change_default_post_per_page( $per_page, $post_type ) {
 add_action( 'widgets_init', 'lfr_add_header_and_footer_widget_areas', 11 );
 function lfr_add_header_and_footer_widget_areas(){
 	register_sidebar( array(
-		'name' => __( 'Header', 'lfr' ),
-		'id' => 'sidebar-header',
-		'description' => esc_html__( 'Appears in the top right side', 'lfr' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-	register_sidebar( array(
 		'name' => __( 'Footer', 'lfr' ),
 		'id' => 'sidebar-footer',
 		'description' => esc_html__( 'Appears in the footer', 'lfr' ),
